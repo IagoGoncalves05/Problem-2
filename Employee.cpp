dcdfc0b9de59be113dee324853b67a15cff290c2
IagoGoncalves05 <>
2021-05-02 12:44:07 +0000

    Employee cpp file added

---
+++ Employee.cpp
@@ -0,0 +1,44 @@
+#include "Employee.h"
+#include <iostream>
+#define quote(x) #x
+
+Employee::Employee(string name) {
+	setName(name);
+}
+
+Employee::Employee(string name, double salary, string office, MyDate date) {
+	setName(name);
+	this->salary = salary;
+	this->office = office;
+	this->dateHired = date;
+}
+
+void Employee::setSalary(double salary) {
+	this->salary = salary;
+}
+
+void Employee::setOffice(string office) {
+	this->office = office;
+}
+
+void Employee::setDateHired(MyDate dateHired){
+	this->dateHired = dateHired;
+}
+
+double Employee::getSalary() {
+	return salary;
+}
+
+string Employee::getOffice() {
+	return office;
+}
+
+MyDate Employee::getDateHired() {
+	return dateHired;
+}
+
+string Employee::toString() const {
+	typeid(Employee).name();
+	return "Name: " + getName() + "\nClass: " + quote(Employee);
+}
+
