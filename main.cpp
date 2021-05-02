acd50d80147bcaff3fe83e50a61ef1a3dad7e393
IagoGoncalves05 <>
2021-05-02 12:46:04 +0000

    output the Student and Employee constructo

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include "Person.h"
-
+#include "Student.h"
+#include "Employee.h"
 using namespace std;
 
 int main()
 {
-    Person person("Iago", "Brazil", "(51)33246057", "iago@email.com");
-    cout << person.toString() << endl;
+    Person p("Iago", "Brazil", "(51)33246057", "iago@email.com");
+    cout << p.toString() << endl;
+
+    cout << endl;
+
+    Student s("Iago", "Sophomore");
+    cout << s.toString() << endl;
+
+    cout << endl;
+
+    Employee e("Iago");
+    cout << e.toString() << endl;
 }
